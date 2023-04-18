29d44fd52b3dcbd054b372d14f82414fb7f4fb9d
maksim <mshelganov@gmail.com>
2023-04-18 04:33:44 +0300

    Added SplitString problem

---
+++ Alalgorithmic.Lib/SplitString.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Alalgorithmic.Lib
+{
+    public static class SplitString
+    {
+        public static string[] Solution(string str)
+        {
+            str = str.Length % 2 == 1 ? str + "_" : str;
+            var count = str.Length / 2;
+            var charArray = new string[count];
+            for (var i = 0; i < count; i++)
+            {
+                charArray[i] = str.Substring(i * 2, 2);
+            }
+
+            return charArray;
+        }
+    }
+}
+
---
+++ Alalgorithmic.Test/SplitStringTests.cs
@@ -0,0 +1,26 @@
+﻿using Alalgorithmic.Lib;
+using NUnit.Framework;
+
+namespace Alalgorithmic.Test;
+
+[TestFixture]
+public class SplitStringTests
+{
+    /// <summary>
+    /// Complete the solution so that it splits the string into pairs of two characters.
+    /// If the string contains an odd number of characters then it should replace
+    /// the missing second character of the final pair with an underscore ('_').
+    /// Examples:
+    ///     * 'abc' =>  ['ab', 'c_']
+    ///     * 'abcdef' => ['ab', 'cd', 'ef']
+    /// </summary>
+    [Test]
+    public void BasicTests()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(SplitString.Solution("abc"), Is.EqualTo(new string[] { "ab", "c_" }));
+            Assert.That(SplitString.Solution("abcdef"), Is.EqualTo(new string[] { "ab", "cd", "ef" }));
+        });
+    }
+}
