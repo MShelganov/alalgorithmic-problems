9c52abee97b13adee7518a40b367fc00b20f80d1
maksim <mshelganov@gmail.com>
2023-04-23 17:02:57 +0300

    implemented regular expression matching solution

---
+++ Alalgorithmic.Lib/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Alalgorithmic.Lib
+{
+	public static class Solution
+	{
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    var sum = nums[i] + nums[j];
+                    if (sum == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int dividedByTen = x;
+            int reversed = 0;
+            while (dividedByTen > 0)
+            {
+                int digit = dividedByTen % 10;
+                reversed = reversed * 10 + digit;
+                dividedByTen /= 10;
+            }
+
+            return x == reversed;
+        }
+
+        public static bool IsMatch(string s, string p)
+        {
+            int m = s.Length;
+            int n = p.Length;
+            bool[,] dp = new bool[m + 1, n + 1];
+            dp[0, 0] = true;
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j - 1] == '*')
+                {
+                    dp[0, j] = dp[0, j - 1];
+                }
+            }
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (p[j - 1] == s[i - 1] || p[j - 1] == '.')
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];
+                    }
+                    else if (p[j - 1] == '*')
+                    {
+                        dp[i, j] = dp[i - 1, j] || dp[i, j - 1];
+                    }
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
---
+++ Alalgorithmic.Test/SolutionsTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using Alalgorithmic.Lib;
+using NUnit.Framework;
+
+namespace Alalgorithmic.Test
+{
+	[TestFixture]
+	public class SolutionsTest
+	{
+		[Test]
+		public void TwoSumArrayTest1()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(Solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9), Is.EqualTo(new int[] { 0, 1 }));
+                Assert.That(Solution.TwoSum(new int[] { 3, 2, 4 }, 6), Is.EqualTo(new int[] { 1, 2 }));
+                Assert.That(Solution.TwoSum(new int[] { 3, 3 }, 6), Is.EqualTo(new int[] { 0, 1 }));
+                Assert.That(Solution.TwoSum(new int[] { 2, 7, 11, 15, 3 }, 6), Is.EqualTo(Array.Empty<int>()));
+                Assert.That(Solution.TwoSum(new int[] { 3, 2, 3 }, 6), Is.EqualTo(new int[] { 0, 2 }));
+            });
+        }
+
+        [Test]
+        public void IsPalindromeTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(Solution.IsPalindrome(121), Is.True);
+                Assert.That(Solution.IsPalindrome(-121), Is.False);
+                Assert.That(Solution.IsPalindrome(123), Is.False);
+                Assert.That(Solution.IsPalindrome(10), Is.False);
+            });
+        }
+
+        /// <summary>
+        /// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+        ///  - '.' Matches any single character.​​​​
+        ///  - '*' Matches zero or more of the preceding element.
+        ///  The matching should cover the entire input string (not partial).
+        /// </summary>
+        [Test]
+        public void RegularExpressionMatching()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(Solution.IsMatch("ab", "a"), Is.False);
+                Assert.That(Solution.IsMatch("aa", "a*"), Is.True);
+                Assert.That(Solution.IsMatch("ab", ".*"), Is.True);
+                Assert.That(Solution.IsMatch("abc", "a.."), Is.True);
+                Assert.That(Solution.IsMatch("abc", ".b."), Is.True);
+            });
+        }
+    }
+}
+
