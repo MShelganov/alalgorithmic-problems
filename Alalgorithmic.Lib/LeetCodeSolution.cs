61442b1f3c182ef007b583c1e8f0e9981e1e6fa5
maksim <mshelganov@gmail.com>
2023-04-24 03:20:59 +0300

    Added Roman numbers solutions

---
+++ Alalgorithmic.Lib/LeetCodeSolution.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alalgorithmic.Lib
+{
+    public static class LeetCodeSolution
+    {
+        public static string IntToRoman(int num)
+        {
+            var romanNumbersDictionary = new Dictionary<string, int>
+            {
+                {
+                    "I", 1
+                },
+                {
+                    "IV", 4
+                },
+                {
+                    "V", 5
+                },
+                {
+                    "IX", 9
+                },
+                {
+                    "X", 10
+                },
+                {
+                    "XL", 40
+                },
+                {
+                    "L", 50
+                },
+                {
+                    "XC", 90
+                },
+                {
+                    "C", 100
+                },
+                {
+                    "CD", 400
+                },
+                {
+                    "D", 500
+                },
+                {
+                    "CM", 900
+                },
+                {
+                    "M", 1000
+                }
+            };
+
+            var tempNumber = num;
+            var result = new StringBuilder();
+            foreach (var roman in romanNumbersDictionary.Reverse())
+            {
+                if (tempNumber <= 0)
+                    break;
+
+                while (tempNumber >= roman.Value)
+                {
+                    result.Append(roman.Key);
+                    tempNumber -= roman.Value;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static int RomanToInt(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            var romanValues = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+
+            int result = 0;
+            int prev = romanValues[s[0]];
+            for (int i = 1; i < s.Length; i++)
+            {
+                int current = romanValues[s[i]];
+                if (current > prev)
+                {
+                    result -= prev;
+                }
+                else
+                {
+                    result += prev;
+                }
+
+                prev = current;
+            }
+
+            result += prev;
+            return result;
+        }
+    }
+}
+
---
+++ Alalgorithmic.Test/LeetCodeSolutionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Alalgorithmic.Lib;
+using NUnit.Framework;
+
+namespace Alalgorithmic.Test
+{
+    [TestFixture]
+    public class LeetCodeSolutionTest
+    {
+        [Test]
+        public void IntToRomanTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(LeetCodeSolution.IntToRoman(3), Is.EqualTo("III"));
+                Assert.That(LeetCodeSolution.IntToRoman(58), Is.EqualTo("LVIII"));
+                Assert.That(LeetCodeSolution.IntToRoman(1994), Is.EqualTo("MCMXCIV"));
+                Assert.That(LeetCodeSolution.IntToRoman(4), Is.EqualTo("IV"));
+                Assert.That(LeetCodeSolution.IntToRoman(5), Is.EqualTo("V"));
+            });
+        }
+
+        [Test]
+        public void RomanToIntTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(LeetCodeSolution.RomanToInt("III"), Is.EqualTo(3));
+                Assert.That(LeetCodeSolution.RomanToInt("LVIII"), Is.EqualTo(58));
+                Assert.That(LeetCodeSolution.RomanToInt("MCMXCIV"), Is.EqualTo(1994));
+                Assert.That(LeetCodeSolution.RomanToInt("IV"), Is.EqualTo(4));
+                Assert.That(LeetCodeSolution.RomanToInt("V"), Is.EqualTo(5));
+            });
+        }
+    }
+}
